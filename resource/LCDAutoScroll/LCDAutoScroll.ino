f220eb7f348a259b05f416ab78f99239d6bad1a2
matzTada <matz@west.sd.keio.ac.jp>
2017-04-27 13:26:49 +0900

    fdsa

--- resource/LCDAutoScroll/LCDAutoScroll.ino
+++ resource/LCDAutoScroll/LCDAutoScroll.ino
@@ -1,6 +1,5 @@
-#include "lcd.h"
-
-
+#include<LiquidCrystal.h>
+#define LCD_COLS 16
 #define LCD_RS_PIN 3
 #define LCD_RW_PIN A1
 #define LCD_ENABLE_PIN 2
@@ -8,17 +7,27 @@
 #define LCD_D5_PIN A2
 #define LCD_D6_PIN A5
 #define LCD_D7_PIN A3
-
-MyLCD mylcd((int)LCD_RS_PIN, (int)LCD_RW_PIN, (int)LCD_ENABLE_PIN, (int)LCD_D4_PIN, (int)LCD_D5_PIN, (int)LCD_D6_PIN, (int)LCD_D7_PIN);
-//LiquidCrystal lcd(LCD_RS_PIN, LCD_RW_PIN, LCD_ENABLE_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
+String lcdStr;
+int lcdStrStartPosition;
+LiquidCrystal lcd(LCD_RS_PIN, LCD_RW_PIN, LCD_ENABLE_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 /*LiquidCrystal(rs(4), rw(5), enable(6), d4(11), d5(12), d6(13), d7(14))
    = ( 4, 5, 6, 11, 12, 13, 14) in LCD
 */
 
 void setup() {
-  mylcd.initialize();
+  lcd.begin(LCD_COLS, 2);
+  lcdStr = "hello hoge humu nyann nyann ";
+  lcdStrStartPosition = 0;
 }
 
 void loop() {
-  mylcd.scroll();
+  for (int i = 0; i < LCD_COLS; i++) {
+    lcd.setCursor(i, 0);
+    int tempPos = i + lcdStrStartPosition;
+    if (tempPos >= lcdStr.length()) tempPos = tempPos - lcdStr.length();
+    lcd.print(lcdStr.charAt(tempPos));
+  }
+  lcdStrStartPosition++;
+  if (lcdStrStartPosition >= lcdStr.length()) lcdStrStartPosition = 0;
+  delay(200);
 }
